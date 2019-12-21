8804dbb14a4da38fe6df2b4210f50206edb2d91c
Roy Yosef <royosef10@gmail.com>
2019-12-21 19:18:09 +0200

    feat(wordsAnalyzer): average words length,  words length standard deviation, DeJargonizer score, DeJargonizer rare words percentage

---
+++ Crawler/Exceptions/StandardDeviationInvalidArgumentsAmountException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Crawler.Exceptions
+{
+	[Serializable]
+	public class StandardDeviationInvalidArgumentsAmountException : Exception
+	{
+		public static StandardDeviationInvalidArgumentsAmountException CreateInstance()
+		{
+			return new StandardDeviationInvalidArgumentsAmountException();
+		}
+
+		public StandardDeviationInvalidArgumentsAmountException()
+		{
+		}
+
+		public StandardDeviationInvalidArgumentsAmountException(string? message) : base(message)
+		{
+		}
+
+		public StandardDeviationInvalidArgumentsAmountException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+		protected StandardDeviationInvalidArgumentsAmountException(SerializationInfo serializationInfo, StreamingContext streamingContext) : base(serializationInfo, streamingContext)
+		{
+
+		}
+	}
+}
