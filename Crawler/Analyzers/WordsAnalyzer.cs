3ff37562f68755a7d42ee7b7e2729a2c7c187253
adibr <adibrill15@gmail.com>
2019-12-24 09:55:48 +0200

    wordsAnalyzer-added-functions
    CalculateEmotionWords + Emotions File
    CalculateQuestionWords
    CalculateNumbersAsDigits
    CalculateNumbersAsWords
    
    Plus test functions in test class

--- Crawler/Analyzers/WordsAnalyzer.cs
+++ Crawler/Analyzers/WordsAnalyzer.cs
@@ -5,6 +5,7 @@ using Crawler.DeJargonizer;
 using Crawler.Exceptions;
 using Crawler.ExtensionMethods;
 using Crawler.LexicalAnalyzer;
+using System.IO;
 
 namespace Crawler.Analyzers
 {
@@ -42,5 +43,64 @@ namespace Crawler.Analyzers
 
 			return deJargonizer.Analyze(words);
 		}
+
+		/// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tokens"></param>
+        /// <returns></returns>
+        public int CalculateNumbersAsDigits(IEnumerable<Token> tokens)
+        {
+            var words = tokens.GetValuesByTokenType(eTokenType.StringValue);
+            return words.Count(w => w.All(l => char.IsDigit(l)));
+            
+        }
+
+        /// <summary>
+        /// count numbers which appear as digits
+        /// </summary>
+        /// <param name="tokens"></param>
+        /// <returns></returns>
+        public int CalculateNumbersAsWords(IEnumerable<Token> tokens)
+        {
+            var DigitStrings = new List<string>() { "zero", "one", "two", "three", "four", "five", "six", "seven", 
+                "eight", "nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen",
+                "eighteen", "nineteen", "twenty","thirty","fourty", "fifty","sixty","seventy","eighty","ninty",
+                "hundred","thousand","million","billion" };
+            var words = tokens.GetValuesByTokenType(eTokenType.StringValue);
+            return words.Count(w=>DigitStrings.Any(D=>w.ToLower().Equals(D)));
+           
+
+        }
+
+        /// <summary>
+        /// count numbers which appear as words
+        /// </summary>
+        /// <param name="tokens"></param>
+        /// <returns></returns>
+        public double CalculateEmotionWords(IEnumerable<Token> tokens)
+        {
+            var words = tokens.GetValuesByTokenType(eTokenType.StringValue);
+            var Filelines = File.ReadAllLines("Emotion.csv");
+            var EmotionStrings = new List<string>(){};
+
+            foreach(string line in Filelines)
+            {
+                EmotionStrings.Add(line.ToLower());
+            }
+
+            return ((double)(words.Count(w => EmotionStrings.Any(E => w.ToLower().Equals(E)))) / (double)words.Count()) ;
+        }
+
+        public int CalculateQuestionWords(IEnumerable<Token> tokens)
+        {
+            var words = tokens.GetValuesByTokenType(eTokenType.StringValue);
+            var QuestionStrings = new List<string>(){"what","when","where","which","who","whom","whose","why", "how" ,"how far",
+                "how long","how many","how much","how old"};
+
+            
+            return words.Count(w=>QuestionStrings.Any(E=>w.Equals(E)));
+            
+        }
 	}
 }
